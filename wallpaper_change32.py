74ac07e33bde2c21f8549e0d370af17bb8971c53
Alexander <cawasp@gmail.com>
2019-11-10 21:22:47 -0800

    main file 32 bit

---
+++ wallpaper_change32.py
@@ -0,0 +1,12 @@
+import ctypes, os, random
+SPI_SETDESKWALLPAPER = 20
+wallpapers = []
+wallpaper = None
+path = os.getcwd()
+
+for filename in os.listdir(os.getcwd()):
+    if not filename.endswith(".py"):
+        wallpapers.append(filename)
+
+wallpaper = random.choice(wallpapers)
+ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, path+"\\"+wallpaper , 3)
