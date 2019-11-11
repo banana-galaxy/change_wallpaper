eb19e28c5d9a291057fa293f4972749e398b3fb8
Alexander <cawasp@gmail.com>
2019-11-11 07:43:17 -0800

    fixed running bug

--- wallpaper_change64.py
+++ wallpaper_change64.py
@@ -2,7 +2,7 @@ import ctypes, os, random
 SPI_SETDESKWALLPAPER = 20
 wallpapers = []
 wallpaper = None
-path = os.getcwd()
+path = os.path.dirname(os.path.realpath(__file__))
 
 for filename in os.listdir(os.getcwd()):
     if not filename.endswith(".py"):
