0cb50eedbf70a42f8480c12bab01978660fb1e2c
Alexander <cawasp@gmail.com>
2019-11-13 17:41:12 -0800

    different file check

--- wallpaper_change_ubuntu.py
+++ wallpaper_change_ubuntu.py
@@ -1,7 +1,7 @@
 import os, random
 
 path = os.path.dirname(os.path.realpath(__file__))
-image_types = ['.jpg', '.jpeg', '.png', '.gif', '.jfif']
+image_types = ['.jpg', '.jpeg', '.png', '.gif']
 wallpapers = []
 wallpaper = None
 
