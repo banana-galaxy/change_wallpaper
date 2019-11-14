ddea5f8b847e57c021534884de79bfbe7e533a3d
Alexander <cawasp@gmail.com>
2019-11-13 17:36:39 -0800

    different file check

--- wallpaper_change32.py
+++ wallpaper_change32.py
@@ -1,12 +1,14 @@
 import ctypes, os, random
 SPI_SETDESKWALLPAPER = 20
+image_types = ['.jpg', '.jpeg', '.png', '.gif', '.jfif']
 wallpapers = []
 wallpaper = None
 path = os.path.dirname(os.path.realpath(__file__))
 
 for filename in os.listdir(os.getcwd()):
-    if not filename.endswith(".py"):
-        wallpapers.append(filename)
+    for image_type in image_types:
+        if filename.endswith(image_type):
+            wallpapers.append(filename)
 
 wallpaper = random.choice(wallpapers)
 ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, path+"\\"+wallpaper , 3)
